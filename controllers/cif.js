80d1898704e5eb53811d48203fae5601ae0da0d3
AidenValley <jhyjsh953@gmail.com>
2022-09-26 23:58:09 -0700

    cif page styling added

--- controllers/cif.js
+++ controllers/cif.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req, res) => {
-  res.render('cif/index', {layout:false});
+  res.render('cif/index');
 });
 
 module.exports = router;
